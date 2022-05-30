80e0e92603c487b27479ae9b5ecefd4f0741e481
Belakhvostau Aliaksandr <aliaksandr@wsa-089-23b.local>
2022-05-30 14:43:17 +0300

    created useHandlers hook

--- src/pages/TodoPage/TodoPageContainer.tsx
+++ src/pages/TodoPage/TodoPageContainer.tsx
@@ -5,6 +5,7 @@ import { ITodo, IPureTodo } from '../../models/ITodo';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { useActions } from '../../hooks/useActions';
 import { availiableOptionsEnum } from '../../models/enums';
+import { useHandlers } from '../../hooks/useHandlers';
 
 const todoOptionsBackend: Record<string, string> = {
   [availiableOptionsEnum.todo]: 'todo',
@@ -14,71 +15,25 @@ const todoOptionsBackend: Record<string, string> = {
 
 const availiableOptions = ['todo', 'in_progress', 'done'];
 
-const defaultValue = {
-  id: '',
-  title: '',
-  description: '',
-  status: '',
-};
-
 export const TodoPageContainer: React.FC = () => {
   const [filteredTodos, setFilteredTodos] = useState([] as ITodo[]);
-  const [initialValue, setInitialValue] = useState(defaultValue as ITodo);
-
   const { isVisible, type } = useTypedSelector((state) => state.modalReducer);
-  const { setVisible, setType } = useActions();
   const { todos, filter } = useTypedSelector((state) => state.todosReducer);
+  const { fetchTodos, setFilter } = useActions();
   const {
-    fetchTodos,
-    setFilter,
-    addTodoActionCreator,
-    deleteTodoActionCreator,
-    updateTodoActionCreator,
-    updateTodoStatusActionCreator,
-  } = useActions();
+    addTodo,
+    deleteTodo,
+    editTodo,
+    handleTodoProgress,
+    handleVisibleModal,
+    initialValue,
+  } = useHandlers();
 
   useEffect(() => {
     fetchTodos();
   }, []);
 
   const hasTodo = !!filteredTodos.length;
-
-  const addTodo = async (todo: IPureTodo) => {
-    addTodoActionCreator(todo);
-    setVisible(false);
-  };
-
-  const deleteTodo = async (id: string) => {
-    const currentTodo = todos.find((todo) => todo.id === id);
-
-    deleteTodoActionCreator(currentTodo!);
-  };
-
-  const handleVisibleModal = (id?: string) => {
-    const isEdit = typeof id === 'string';
-    let defaultValue = {
-      id: '',
-      title: '',
-      description: '',
-      status: '',
-    };
-
-    setVisible(true);
-    setType(isEdit ? 'edit' : 'create');
-
-    if (isEdit) {
-      const currTodo = todos.find((todo) => todo.id === id);
-      defaultValue = { ...currTodo! };
-    }
-
-    setInitialValue(defaultValue);
-  };
-
-  const handleTodoProgress = async (id: string, status: string) => {
-    const currentTodo = todos.find((todo) => todo.id === id);
-
-    updateTodoStatusActionCreator({ ...currentTodo, status: status });
-  };
 
   const filterTodos = useCallback(
     (title: string) => {
@@ -95,19 +50,6 @@ export const TodoPageContainer: React.FC = () => {
   useEffect(() => {
     filterTodos(filter);
   }, [todos, filter, filterTodos]);
-
-  const editTodo = async (todo: IPureTodo) => {
-    const { id } = initialValue;
-    const currTodo = todos.find((todo) => todo.id === id);
-    const editedTodo = {
-      ...currTodo!,
-      ...todo,
-    };
-
-    updateTodoActionCreator(editedTodo);
-
-    setVisible(false);
-  };
 
   const handleSubmit = (data: IPureTodo) =>
     type === 'create' ? addTodo(data) : editTodo(data);
